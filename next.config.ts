ed3a5338f1ae9bf0a36fb0a4964268ca61d40ab8
gowshikawebronic <gowshika.webronic@gmail.com>
2026-01-24 17:14:53 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+    
+ basePath: "/storetech-portfolio",
+output: "export",
 };
 
 export default nextConfig;
